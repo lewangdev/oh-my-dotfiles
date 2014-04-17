a23efc425a4d50d8339d5e80f2801abc9eba5a8e
Le Wang <thisiswangle@gmail.com>
2014-04-17 22:51:14 +0800

    upload two schemes

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+[ -e '~/.zshrc' ] && mv ~/.zshrc ~/.zshrc.orig
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+
+[ -e '~/.vimrc' ] && mv ~/.vimrc ~/.vimrc.orig
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
+
+[ -e '~/.vim' ] && mv ~/.vim ~/.vim.orig
+ln -s ~/.dotfiles/.vim ~/.vim

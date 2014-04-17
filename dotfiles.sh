990a7e7d629f580db99f3d6b664f9a085c93bcc4
Le Wang <thisiswangle@gmail.com>
2014-04-18 00:01:07 +0800

    update ssh

--- dotfiles.sh
+++ dotfiles.sh
@@ -8,3 +8,12 @@ ln -s ~/.dotfiles/.vimrc ~/.vimrc
 
 [ -e '~/.vim' ] && mv ~/.vim ~/.vim.orig
 ln -s ~/.dotfiles/.vim ~/.vim
+
+[ -e '~/.tmunx.conf' ] && mv ~/.tmunx.conf ~/.tmunx.conf.orig
+ln -s ~/.dotfiles/.tmunx.conf .tmunx.conf
+
+[ -e '~/.pip' ] && mv ~/.pip ~/.pip.orig
+ln -s ~/.dotfiles/.pip .pip
+
+[ -e '~/.ssh' ] && mv ~/.ssh ~/.ssh.orig
+ln -s ~/.dotfiles/.ssh .ssh

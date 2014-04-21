a8b30a56876d7602031df1d85bf1114fc64b5a87
Le Wang <thisiswangle@gmail.com>
2014-04-21 13:26:02 +0800

    update tmux

--- tmuxenv.sh
+++ tmuxenv.sh
@@ -3,26 +3,20 @@
 # Initialize tmux env for work
 #
 TMUX=$(which tmux)
-SESSION=4tmux
+SESSION=workon
 
 if [ -z $TMUX ]; then
     echo "You need to install tmux."
     exit 1
 fi
 
-$TMUX has -t $SESSION
+$TMUX has-session -t $SESSION
 
 if [ $? != 0 ]; then
-    $TMUX new -d -n vim -s $SESSION "vim"
-    $TMUX splitw -v -p 20 -t $SESSION "pry"
-    $TMUX neww -n mutt -t $SESSION "mutt"
-    $TMUX neww -n irssi -t $SESSION "irssi"
-    $TMUX neww -n cmus -t $SESSION "cmus"
-    $TMUX neww -n zsh -t $SESSION "zsh"
-    $TMUX splitw -h -p 50 -t $SESSION "zsh"
-    $TMUX selectw -t $SESSION:5
+    $TMUX new-session -d -n workonw -s $SESSION
+    $TMUX select-window -t $SESSION:1
 fi
 
-$TMUX att -t $SESSION
+$TMUX attach -t $SESSION
 
 exit 0
